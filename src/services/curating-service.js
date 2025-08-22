f858ad4c0c7b724d794ad9515c8d1a98a15dffad
kokooa <kokooa>
2025-08-22 16:27:35 +0900

    frist edit

--- src/services/curating-service.js
+++ src/services/curating-service.js
@@ -1,75 +1,45 @@
 import prisma from "../client/prisma-client.js";
-import bcrypt from 'bcrypt'
 
-export class CurationService {
-    //암호화 처리 부분 (post만 암호화 처리 하기 ..)
-    async createCuration(data) {
-        const { password, ...restOfData } = data;
-        console.log('암호화 전 비밀번호:', password);
-
-        const saltRound = 10; // 암호 보안 수준 정도 설정 (10-12가 적당하다고 함)
-        const hashedPassword = await bcrypt.hash(password, saltRound);
-
-        console.log('암호화 후 비밀번호:', hashedPassword);
-
-        return await prisma.curation.create({
-            data: {
-                ...restOfData,
-                password: hashedPassword,
-            },
-        });
+export const getCuration = async (styleId) => {
+  if (isNaN(styleId)) {
+      const err = new Error("잘못된 요청입니다");
+      err.status = 400;
+      throw err;
+  }
+  
+  return await prisma.style.findUnique({
+    where: { id: styleId },
+    include: {
+      curations: true,   
+      _count: { select: { curations: true } } 
     }
+  });
+};
 
-    // 큐레이팅 목록 조회하기 
-    async getCurations(styleId, page, pageSize, searchBy, keyword) {
-        try {
-            const searchContent = {};
-            if (searchBy && keyword) {
-                if (searchBy === "nickname" || searchBy === "content") {
-                    searchContent[searchBy] = {
-                        contains: keyword,
-                    };
-                }
-            }
 
-            const [totalItems, curations] = await prisma.$transaction([
-                prisma.curation.count({
-                    where: { styleId, ...searchContent },
-                }),
-                prisma.curation.findMany({
-                    where: { styleId, ...searchContent },
-                    skip: (page - 1) * pageSize,
-                    take: pageSize,
-                    include: {
-                        comment: true, // 큐레이션과 연결된 모든 댓글 데이터까지 가져올 수 잇음
-                    },
-                    orderBy: {
-                        createdAt: "desc",
-                    },
-                }),
-            ]);
-            console.log(curations);
-            const totalPages = Math.ceil(totalItems / pageSize);
+export const postCuration = async (styleId, { nickname, content, password, trendy, personality, practicality, costEffectiveness } ) => {
 
-            return {
-                currentPage: page,
-                totalPages,
-                totalItemCount: totalItems,
-                data: curations,
-            };
-        } catch (error) {
-            console.log("Error fetching curations:", error);
-            return {
-                currentPage: 1,
-                totalPages: 0,
-                totalItemCount: 0,
-                data: [],
-            };
-        }
-    }
-}
+  const postedCuration = await prisma.curation.create({
+      data: {
+        styleId,
+        nickname,
+        content,
+        password,
+        trendy,
+        personality,
+        practicality,
+        costEffectiveness
+    },
+  });
+  
+  const styleWithCurations = await prisma.style.findUnique({
+    where: { id: styleId },
+    include: { curations: true }
+  });
+  return styleWithCurations.curations;
+};
 
-//클래스 객체로 넣기 const putCuration = async (curationId, { password, nickname, content, trendy, personality, practicality, costEffectiveness}) => {
+export const putCuration = async (curationId, { password, nickname, content, trendy, personality, practicality, costEffectiveness}) => {
   
   if (isNaN(curationId)) {
     const err = new Error("잘못된 요청입니다");
@@ -152,4 +122,23 @@ export const deleteCuration = async (curationId, password) => {
   });
   return styleWithCurations.curations;
 };
+
+export const searchCurationsByKeyword = async (keyword) => {
+  if (!keyword) {
+    const err = new Error("검색어가 필요합니다")
+    err.status = 400;
+    throw err;
+}
+
+  
+  const curations = await prisma.curation.findMany({
+    where: {
+      OR: [
+        { nickname: { contains: keyword, mode: "insensitive"} }, 
+        { content: { contains: keyword, mode: "insensitive"} }, 
+      ],
+    },
+  });
+
+  return curations;
 };
