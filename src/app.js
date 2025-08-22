e91801727c678ac7a13ee76f274a6afecc311645
kokooa <ckdwns1379@gmail.com>
2025-08-22 16:35:56 +0900

    first (#8)
    * Feature/lsj (#2)
    
    * lsj : readme.md path
    
    * 이하영추가 (#3)
    
    .
    
    * feat: readme.md file
    
    * edit 22
    
    * Delete src/controllers/ranking-controller.js
    
    * Delete src/routers directory
    
    * Delete src/services/style-service.js
    
    * frist edit
    
    ---------
    
    Co-authored-by: morunero211 <151031013+morunero211@users.noreply.github.com>
    Co-authored-by: winnie4869 <lhy6598@naver.com>
    Co-authored-by: mone <qhdltmwhs@gmail.com>
    Co-authored-by: qhdltmwhs <75594785+qhdltmwhs@users.noreply.github.com>
    Co-authored-by: kokooa <kokooa>

--- src/app.js
+++ src/app.js
@@ -1,39 +1,24 @@
 import express from "express";
+import prisma from "./client/prisma-client.js";
 import cors from "cors";
 import "dotenv/config";
-import curationRouter from './routers/curating-router.js';
-import { errorHandler } from "./middilewares/errorHandler.js";
 import path from "path"; 
 import { fileURLToPath } from "url"; 
-
-// import styleRouter from "./routers/style-router.js";
+import curatingRouter from "./routers/curating-router.js";
+// import errorHandler from "./middlewares/errorHandler-middleware.js";
+// import styleRouter from "./routers/styles-router.js";
+// import commentsRouter from "./routers/comments-rotuer.js";
+// import imagesRouter from "./routers/images-router.js";
+// import tagsRouter from "./routers/tags-router.js"
 // import rankingRouter from "./routers/ranking-router.js";
-// import commentRouter from "./routers/comment-router.js";
-// import tagRouter from "./routers/tag-router.js";
-// import imageRouter from "./routers/image-router.js";
-
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cors({
     origin: "*"
 }));
-
-app.use(process.env.STATIC_FILE_PATH || '/files', express.static(path.join(__dirname, 'uploads')));
-
-app.use('/styles', curationRouter); 
-// app.use("/", styleRouter);
-// app.use("/", rankingRouter);
-// app.use("/", commentRouter);
-// app.use("/", tagRouter);
-// app.use("/", imageRouter);
-app.use(errorHandler);
-
 
 app.get("/", (req, res) => {
     res.send("서버 정상");
@@ -50,10 +35,15 @@ app.all(/(.*)/, (req, res) => {
     res.status(404).send({ message: '요청하신 리소스를 찾을 수 없습니다.' });
 });
 
-// 서버 실행
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+
+// // 애플리케이션 종료 시 Prisma 클라이언트 연결 해제
+// process.on('beforeExit', async () => {
+//   console.log('Server is shutting down. Disconnecting from database...');
+//   await prisma.$disconnect();
+// });
+
+// app.use(errorHandler);
+
 
 // 애플리케이션 종료 시 Prisma 클라이언트 연결 해제
 process.on('beforeExit', async () => {
@@ -61,3 +51,21 @@ process.on('beforeExit', async () => {
   await prisma.$disconnect();
 });
 
+// app.use(errorHandler);
+
+// 서버 실행
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+// async function testDB() {
+//   try {
+//     await prisma.$connect();
+//     console.log("✅ DB 연결 성공!");
+//   } catch (err) {
+//     console.error("❌ DB 연결 실패:", err);
+//   }
+// }
+// testDB();
+
+export default app;
--- src/routers/curating-router.js
+++ src/routers/curating-router.js
@@ -1,12 +1,14 @@
-import { Router } from "express";
+import express from 'express';
+import { validate } from '../middlewares/validators-middleware.js';
+import { createCurationSchema } from '../validators/curating-validator.js';
 import { CurationController } from "../controllers/curating-controller.js";
 
-const router = Router();
-const curationController = new CurationController();
+const router = express.Router();
+const controller = new CurationController();
 
-router.post("/:styleId/curations", curationController.createCuration);
-
-router.get("/:styleId/curations", curationController.getCurations);
-
+router.get("/styles/:styleId/curations", controller.getCurations.bind(controller));
+router.post("/styles/:styleId/curations", validate(createCurationSchema), controller.postCurations.bind(controller));
+router.put("/curations/:curationId", controller.putCurations.bind(controller));
+router.delete("/curations/:curationId", controller.deleteCurations.bind(controller));
+router.get("/curations/search", controller.searchCurations.bind(controller)) 
 export default router;
-
