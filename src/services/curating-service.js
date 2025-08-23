b9cef51a7320c50452aff70796213d62aca99e7a
kimhyeyeon <kho1039@naver.com>
2025-08-23 16:12:43 +0900

    ho

--- src/services/curating-service.js
+++ src/services/curating-service.js
@@ -27,30 +27,34 @@ export class CurationService {
                 }
             }
 
-        const [totalItems, curations] = await prisma.$transaction([
-            prisma.curation.count({
-                where: { styleId, ...searchContent},
-            }),
-            prisma.curation.findMany({
-                where: { styleId, ...searchContent },
-                skip: (page - 1) * pageSize,
-                take: pageSize,
-                include: {
-                comment: true, // 큐레이션과 연결된 모든 댓글 데이터까지 가져올 수 잇음
-                },
-                orderBy: {
-                    createdAt: "desc",
-                },
-            }),
-        ]);
+            const [totalItems, curations] = await prisma.$transaction([
+                prisma.curation.count({
+                    where: { styleId, ...searchContent },
+                }),
+                prisma.curation.findMany({
+                    where: { styleId, ...searchContent },
+                    skip: (page - 1) * pageSize,
+                    take: pageSize,
+                    include: {
+                        comment: true, // 큐레이션과 연결된 모든 댓글 데이터까지 가져올 수 잇음
+                    },
+                    orderBy: {
+                        createdAt: "desc",
+                    },
+                }),
+            ]);
 
-        const totalPages = Math.ceil(totalItems / pageSize);
+            const totalPages = Math.ceil(totalItems / pageSize);
 
-        return {
-            currentPage: page,
-            totalPages,
-            totalItemCount: totalItems,
-            data: curations,
-        };
+            return {
+                currentPage: page,
+                totalPages,
+                totalItemCount: totalItems,
+                data: curations,
+            };
+        } catch (error) {
+            console.log('"Error fetching curations:", error');
+            throw new Error("큐레이션 목록을 가져오는 데 실패했습니다.");
+        }
     }
 }
